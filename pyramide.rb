917ed7365f48d164e741e9e1034cc92c21fba371
MarieDechaux <marie.dechaux@gmail.com>
2020-09-29 20:08:55 +0200

    with while

--- pyramide.rb
+++ pyramide.rb
@@ -3,6 +3,13 @@
 puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
 print "> "
 number = gets.chomp.to_i
+
+while number <1 || number >25
+  puts "NON Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ENTRE 1 ET 25 ?"
+  print "> "
+  number = gets.chomp.to_i
+end 
+
 puts "Voici la pyramide :"
 number.times do |i|
   i += 1 
